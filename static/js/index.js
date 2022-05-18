7a8e6c9dab895ce877ce39c5a0a42eddbab56675
FancyCodeMaster <fancycodemaster@gmail.com>
2022-05-19 02:29:06 +0545

    final touch

--- static/js/index.js
+++
@@ -1,6 +0,0 @@
-const confirmLink = document.querySelector(".confirm_link");
-
-const confirmButton = document.querySelector(".confirm_button");
-
-confirmButton.addEventListener("click" , (e) => {
-})
